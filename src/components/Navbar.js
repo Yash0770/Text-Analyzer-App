7b1e0e49844ca7d3ffa9620a72faace18ffa30ac
Yash <100015221+Yash0770@users.noreply.github.com>
2023-04-11 18:26:59 +0530

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
+
+export default function Navbar(props) {
+  return (
+    <nav className={`navbar light navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+    <div className="container-fluid">
+      <Link className="navbar-brand brand-name" to="/">{props.title}</Link>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+            <Link className="nav-link active newFont" aria-current="page" to="/">Home</Link> 
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link newFont" to="/about">{props.aboutText}</Link>
+          </li>
+        </ul>
+
+          {/* Dropdown End */}
+          <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
+          <input className="form-check-input newFont" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+          <label className="form-check-label newFont" htmlFor="flexSwitchCheckDefault">{props.modeModeText}</label>
+        </div>
+      </div>
+    </div>
+  </nav>
+  )
+}
+
+Navbar.propTypes = {
+    title : PropTypes.string.isRequired,
+    aboutText : PropTypes.string.isRequired
+}
+
+Navbar.defaultProps = {
+    title : "Default Title",
+    aboutText : "Default About"
+}
